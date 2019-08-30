c0f56dfcafaa7162c5a353517984a46e5924a433
ZhamMichelle <kettebekovanyc@gmail.com>
2019-08-30 22:14:47 +0600

    два java файла.

---
+++ src/com/levelp/jb2/BigDream.java
@@ -0,0 +1,7 @@
+package com.levelp.jb2;
+
+public class BigDream {
+    public static void main(String[] args) {
+        System.out.println("Never give up!");
+    }
+}
---
+++ src/com/levelp/jb2/BigDream1.java
@@ -0,0 +1,7 @@
+package com.levelp.jb2;
+
+public class BigDream1 {
+    public static void main(String[] args) {
+        System.out.println("You are strong!");
+    }
+}
